2ca2f92e532e5785c4604485358d054b39ace58c
huangsiyuan <huangsiyuan@csiisz.com>
2020-11-18 17:40:09 +0800

    gitconfig更新test

--- src/vdom/index.js
+++ src/vdom/index.js
@@ -3,18 +3,17 @@ import {isObject, isReservedTag} from '../util'
 export function createElement(vm, tag, data={}, ...children) {
 
     // 需要对标签名做过滤 因为有可能是自定义组件
-    // if (isReservedTag(tag)) {
+    if (isReservedTag(tag)) {
         return vnode(vm, tag, data, data.key, children, undefined)
-    // }else {
-    //     // 自定义组件
-    //     const Ctor = vm.$options.components[tag] // 可能是组件或者对象
-    //     return createComponent(vm,tag,data,data.key,children,Ctor)
-    // }
+    }else {
+        // 自定义组件
+        const Ctor = vm.$options.components[tag] // 可能是组件或者对象
+        return createComponent(vm,tag,data,data.key,children,Ctor)
+    }
 }
 function createComponent(vm,tag,data,key,children,Ctor) {
     console.log(vm.$options)
     if (isObject(Ctor)) Ctor = vm.$options._base.extend(Ctor) // 对象即生成一个函数
-    
     data.hook = {
         init(vnode){
             let child = vnode.componentInstance = new vnode.componentOptions.Ctor({})
